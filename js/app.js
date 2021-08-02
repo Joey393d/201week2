cf54cd4bbde3e7f6397f666ecebe552fac59ae43
Joey <joseph.r.young2002@gmail.com>
2021-08-02 12:13:08 -0500

    More cat things

--- js/app.js
+++ js/app.js
@@ -22,3 +22,36 @@ function randomAge(min, max) {
 
 petOne.setAge();
 petOne.isFixed = false;
+console.log(petOne);
+
+let parentElement = document.getElementById('kittenProfiles')
+
+let article = document.createElement("article");
+
+parentElement.appendChild(aricle);
+
+let h2 = document.createElement('h2');
+h2.textContent = petOne.name;
+console.log(h2)
+article.appendChild(h2);
+let petPara = document.createElement('p')
+petPara.textContent = 'Cats are fun, and this cat is ' + petOne.age + ' old.';
+
+
+article.appendChild(petPara);
+let petUL = document.createElement('ul');
+article.appendChild(petUL);
+
+
+for(let i = 0; i < petOne.interests.length; i++){
+  let petLI = document.createElement('li');
+  console.log(petLi)
+  petLi.textContent = petOne.interests[i];
+  petUL.appendChild(petLi);
+
+}  
+
+let petOneImg = document.createElement('img');
+petOneImg.setAttribute("src", "images/" + petOne.imageName + '.jpg');
+petOneImg.setAttribute("alt", "Picture of kitten to adopt.");
+article.appendChild(petOneImg);
